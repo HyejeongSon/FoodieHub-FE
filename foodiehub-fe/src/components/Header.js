293cc38dcb0882ce2cc6273d217ab344e5f46437
taeyeon030725 <taeyeon030725@gmail.com>
2024-12-27 17:30:56 +0900

    Header 완성

--- foodiehub-fe/src/components/Header.js
+++ foodiehub-fe/src/components/Header.js
@@ -1,8 +1,9 @@
 // src/components/Header.js
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {httpRequest} from "../store/httpRequest";
+import { httpRequest } from "../store/httpRequest";
 import { useUser } from "../contexts/UserContext"; // Context 추가
+import "./Header.css";
 
 const Header = () => {
     const navigate = useNavigate();
@@ -14,6 +15,11 @@ const Header = () => {
         console.log("Header에서 Context 상태 업데이트 호출됨");
     }, []);
 
+    // 네비게이션 함수
+    const handleNavigate = (path) => {
+        navigate(path);
+    };
+
     // **2. API 호출 및 사용자 상태 업데이트**
     useEffect(() => {
         console.log("Header 컴포넌트 useEffect 실행됨");
@@ -23,9 +29,9 @@ const Header = () => {
                 // setUser({email:data.email, role: data.role });
                 setUser({
                     email: userInfo.email,
-                    name : userInfo.name,
-                    nickname : userInfo.nickname,
-                    provider : userInfo.provider,
+                    name: userInfo.name,
+                    nickname: userInfo.nickname,
+                    provider: userInfo.provider,
                     role: userInfo.role || "ROLE_USER",
                 });
 
@@ -55,9 +61,9 @@ const Header = () => {
             // setUser({ username: "", role: "" }); // 사용자 상태 초기화
             setUser({
                 email: "",
-                name : "",
-                nickname : "",
-                provider : "",
+                name: "",
+                nickname: "",
+                provider: "",
                 role: ""
             });
             navigate("/main");
@@ -70,28 +76,32 @@ const Header = () => {
     };
 
     return (
-        <header  style ={{background: "grey"}}>
-            <div onClick={() => navigate("/main")}>
-                <img src="/img/foodieHub.png" // 로고 이미지 경로
-                     alt="foodieHub" width={50} height={50}/>
-            </div>
-            <nav>
-                <button onClick={() => navigate("/")}>홈</button>
+        <header>
+            <div className="checkered-bar"></div>
+            <div className="logo-wrapper">
+                <img
+                    src="/img/foodieHub.png"
+                    alt="foodieHub" width={277} height={195} style={{ margin: '20px auto 0' }}
+                    className="logo"
+                    onClick={() => handleNavigate("/main")}
+                /></div>
+            <nav className="nav">
+                {<button className="button" onClick={() => navigate("/")}>홈</button>}
 
                 {user.email && user.role.split("_").pop() === "USER" &&
-                    <button onClick={() => navigate("/mypage")}>마이페이지</button>
+                    <button className="button" onClick={() => navigate("/mypage")}>마이페이지</button>
                 }
 
                 {user.email && user.role === "ROLE_ADMIN" &&
-                    <button onClick={() => navigate("/mypage")}>관리자마이페이지</button>}
+                    <button className="button" onClick={() => navigate("/mypage")}>관리자마이페이지</button>}
 
 
                 {user.email &&
-                    <button type="button" id="logout-btn" onClick={handleLogout}>로그아웃</button>}
+                    <button className="button" id="logout-btn" onClick={handleLogout}>로그아웃</button>}
 
 
                 {!user.email &&
-                    <button onClick={() => navigate("/login")}>로그인</button>}
+                    <button className="button" onClick={() => navigate("/login")}>로그인</button>}
             </nav>
         </header>
     );
